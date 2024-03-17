ea92f887521abac2493ae27160c5022c15da66d1
Vadim Nazarov <2979713+vadimnazarov@users.noreply.github.com>
2024-03-17 21:44:00 +0200

    refactor: update the dependencies

--- R/immundata-package.R
+++ R/immundata-package.R
@@ -3,8 +3,12 @@
 
 ## usethis namespace: start
 #' @import rlang
+#' @importFrom checkmate assert
 #' @importFrom checkmate assertCharacter
 #' @importFrom checkmate assertR6
+#' @importFrom checkmate checkCharacter
+#' @importFrom checkmate checkChoice
+#' @importFrom checkmate checkR6
 #' @importFrom glue glue
 #' @importFrom lifecycle deprecated
 ## usethis namespace: end
