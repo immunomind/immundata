1ea8da0d28355129e42765c2e85b6ab4f127ce2b
Vadim Nazarov <2979713+vadimnazarov@users.noreply.github.com>
2024-03-17 21:43:40 +0200

    feat: add get_available_engines; wip: experiment with .onLoad for testing purposes

--- R/zzz.R
+++ R/zzz.R
@@ -2,10 +2,18 @@
   message("Loading immundata version ", packageVersion(pkgname))
 
   # Do registry stuff here
+  .BACKEND_REGISTRY_CLASS <- BackendEngineRegistry$new()
+  .BACKEND_REGISTRY_CLASS$register(c("df", "data.frame"), BackendEngine)
+
+  loader <- ImmunDataLoader("./", "df")
 }
 
 .onUnload <- function(libpath) {
   message("Unloading immundata")
 
   # Cleanup created datasets, close connection
+}
+
+get_available_engines <- function() {
+  .BACKEND_REGISTRY_CLASS$engines()
 }
