1f0ed3bfbb31e0861104cb88a5ce42ba152dbd0f
Vadim Nazarov <2979713+vadimnazarov@users.noreply.github.com>
2024-03-17 20:39:07 +0200

    feat: update the dependencies

--- R/immundata-package.R
+++ R/immundata-package.R
@@ -3,6 +3,8 @@
 
 ## usethis namespace: start
 #' @import rlang
+#' @importFrom checkmate assertCharacter
+#' @importFrom checkmate assertR6
 #' @importFrom glue glue
 #' @importFrom lifecycle deprecated
 ## usethis namespace: end
