1ba17720526c0d49546c99094e7255de220e2777
Vadim Nazarov <2979713+vadimnazarov@users.noreply.github.com>
2024-03-17 21:44:33 +0200

    docs: fix the docs for BackendEngineRegistry

--- R/backend-engine-registry-class.R
+++ R/backend-engine-registry-class.R
@@ -7,11 +7,11 @@
 #' @name BackendEngineRegistry
 #' @export
 #' @examples
-    #' registry <- BackendEngineRegistry$new()
-    #' registry$register("myEngine", MyBackendEngine$new())
-    #' immloader <- ImmunDataLoder("path/to/yout/dataset")
-    #' immdata <- ImmunDataLoader$load("myEngine")
-    #' immdata <- ImmunDataLoader$load(MyBackendEngine$new()) # same as above
+#' registry <- BackendEngineRegistry$new()
+#' registry$register("myEngine", MyBackendEngine$new())
+#' immloader <- ImmunDataLoder("path/to/yout/dataset")
+#' immdata <- ImmunDataLoader$load("myEngine")
+#' immdata <- ImmunDataLoader$load(MyBackendEngine$new()) # same as above
 #' @keywords internal
 BackendEngineRegistry <- R6Class("BackendEngineRegistry",
   private = list(
@@ -19,36 +19,45 @@ BackendEngineRegistry <- R6Class("BackendEngineRegistry",
   ),
   public = list(
 
-    #' Object Constructor
-    #'
-    #' Initializes the BackendEngineRegistry object.
+    #' @description Object Constructor
+    #' @details Initializes the BackendEngineRegistry object.
     initialize = function() {},
 
+    #' @description Get a list of engine names
+    #' @return Character vector of engine names.
+    names = function() {
+      names(private$engines)
+    },
+
     #' @description Register a new backend engine.
-    #' @param name The name of the backend engine to register. Must be a non-empty string.
-    #' @param engine An instance of the backend engine to register. Must be a child instance of an R6 class "BackendEngine"
+    #' @param .name The name of the backend engine to register. Must be a non-empty string.
+    #' If provided a vector, use every elements as an alias.
+    #' @param .engine An instance of the backend engine to register. Must be a child instance of an R6 class "BackendEngine"
     #' @return Invisible. The function is called for its side effect of registering an engine.
     #' @examples
     #' registry$register("exampleEngine", ExampleEngine$new())
-    register = function(name, engine) {
-      assertCharacter(name, len = 1, min.chars = 1, add = "Engine name must be a non-empty string.")
-      assertR6(engine, classes = "BackendEngine", add = "Engine must be an instance of BackendEngine.")
+    register = function(.name, .engine) {
+      assertCharacter(.name, min.chars = 1)
+      assertR6(.engine, classes = "BackendEngine")
 
-      private$engines[[name]] <- engine
+      for (engine_alias in .name) {
+        private$engines[[engine_alias]] <- .engine
+      }
     },
 
     #' @description Retrieve a registered backend engine by name.
-    #' @param name The name of the backend engine to retrieve. Must be a non-empty string.
+    #' @param .name The name of the backend engine to retrieve. Must be a non-empty string.
+    #' @param ... Other parameters you pass to the constructor of BackendEngine coded by ".name"
     #' @return An instance of the backend engine.
     #' @examples
     #' engine <- registry$getEngine("exampleEngine")
-    get = function(name) {
-      assertCharacter(name, len = 1, min.chars = 1, add = "Engine name must be a non-empty string.")
+    get = function(.name, ...) {
+      assertCharacter(.name, len = 1, min.chars = 1)
 
-      if (!is.null(private$.engines[[name]])) {
-        return(private$.engines[[name]]$new())
+      if (!is.null(private$.engines[[.name]])) {
+        return(private$.engines[[.name]]$new(...))
       } else {
-        stop("Engine not found: ", name)
+        stop("Engine not found: ", .name)
       }
     }
   )
